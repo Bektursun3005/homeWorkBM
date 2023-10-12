131371036ce97d596431715fe2b65c29c3c10022
Meerim33 <amirem29@gmail.com>
2023-10-12 20:10:20 +0600

    шеыраапв

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        man(25, 45);
+        man(55, -20);
+        man(30, 18);
+        man(2, 30);
+        man(40, -30);
     }
+
+    public static boolean man(int age, int temperature) {
+        if (age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) {
+            System.out.println("mozhno idti gulyat");
+        } else if (age < 20 || temperature >= 0 && temperature >= 28) {
+            System.out.println("mozhno idti gulyat");
+        } else if (age > 45 && temperature >= -10 && temperature <= 25) {
+            System.out.println("mozhno idti gulyat");
+        } else {
+            System.out.println("ostovaytes doma");
+        }
+        return true;
+    }
+
 }
